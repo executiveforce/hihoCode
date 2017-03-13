e3bcf4b00ae2b25e621c6ad9d78e8cd9c6cd5249
李贝贝 <Billy@promote.cache-dns.local>
2017-03-13 21:57:27 +0800

    upload hihocode

---
+++ hihoCode/src/hihoCode/chacter_remove.java
@@ -0,0 +1,29 @@
+package hihoCode;
+
+import java.util.Scanner;
+
+public class chacter_remove {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			char[] chacters = sc.nextLine().toCharArray();
+			for (int j = 0; j < chacters.length+1; j++) {
+				for (int j2 = 0; j2 < chacters.length; j2++) {
+					
+				}
+			}
+		}
+		sc.close();
+	}
+	
+	public static int remove(char[] chacters, char c){
+		for (int i = 0; i < chacters.length+1; i++) {
+			
+		}
+		return 1;
+	}
+
+}
